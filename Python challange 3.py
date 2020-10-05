0b5520861f28f36ac01a7b8f01498a93ec917dbb
Kimfucious <github@kimford.me>
2020-10-05 19:32:49 +0700

    deleted duplicate unneeded files

--- Python challange 3.py
+++
@@ -1,7 +0,0 @@
-# ask the user for the width and lenght of a shape
-L = int( input("please enter the lenght:"))
-W = int( input("please enter the width:"))
-#calculate the area of the shape
-area = L*W
-#print the area of the shape
-print (area)

690ce3b700a1b6f794d1cd2ad880205f0e5fc85d
Junior <junior@Anakin.local>
2020-10-05 20:59:36 +0700

    complete work

--- challenge_3.py
+++ challenge_3.py
@@ -1,11 +1,12 @@
 # ask the user for the width and length of a shape
-length = int( input("Please enter the length: "))
-width = int( input("Please enter the width: "))
-#calculate the area of the shape
-area = length * width 
-#print the area of the shape
-
-# Read the docs on Python String Formatting: 
+length = int(input("Please enter the length: "))
+width = int(input("Please enter the width: "))
+# calculate the area of the shape
+area = length * width
+# print the area of the shape
+# Read the docs on Python String Formatting:
 # https://www.w3schools.com/python/
 # and/or look at examples in challenge 1
-print (area)
+msg = "The area of the shape is {}"
+# The arguments for the format method match the curly braces in msg
+print(msg.format(area))

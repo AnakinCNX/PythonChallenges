bc0ede4ba6fc17e73a8b3faba35b48b3de5a9915
Kimfucious <github@kimford.me>
2020-10-05 19:35:25 +0700

    add comment

---
+++ challenge_3.py
@@ -0,0 +1,11 @@
+# ask the user for the width and length of a shape
+length = int( input("Please enter the length: "))
+width = int( input("Please enter the width: "))
+#calculate the area of the shape
+area = length * width 
+#print the area of the shape
+
+# Read the docs on Python String Formatting: 
+# https://www.w3schools.com/python/
+# and/or look at examples in challenge 1
+print (area)

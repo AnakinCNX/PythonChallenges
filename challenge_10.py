8ed316fc4ba00ee884761a0c79147a872ef089d6
Junior <junior@Anakin.local>
2020-10-14 16:50:23 +0700

    Completed challenges

--- challenge_10.py
+++ challenge_10.py
@@ -1,5 +1,35 @@
-# Spelling!
-#rock paper sisers
+# Make user play rock paper scissors with computer
+
+import random
+users_choice = input("Rock, paper or scissors?: ").lower()
+
+mylist = ["rock", "paper", "scissors"]
+computers_choice = random.choice(mylist)
+msg = "The computer chose {}"
+
+result = ""
+if users_choice not in mylist:
+    print("Invalid input!")
+    exit()
 
 
-
+if computers_choice == "rock" and users_choice == "rock":
+    result = "It's a tie!"
+elif computers_choice == "rock" and users_choice == "paper":
+    result = "You won!"
+elif computers_choice == "rock" and users_choice == "scissors":
+    result = "Computer wins!"
+elif computers_choice == "paper" and users_choice == "paper":
+    result = "It's a tie!"
+elif computers_choice == "paper" and users_choice == "rock":
+    result = "Computer wins!"
+elif computers_choice == "paper" and users_choice == "scissors":
+    result = "You won!"
+elif computers_choice == "scissors" and users_choice == "scissors":
+    result = "It's a tie!"
+elif computers_choice == "scissors" and users_choice == "paper":
+    result = "Computer wins!"
+elif computers_choice == "scissors" and users_choice == "rock":
+    result = "You won!"
+print(msg.format(computers_choice))
+print(result)
--- challenge_8.py
+++ challenge_8.py
@@ -1,34 +1,11 @@
 # write program to change munber into grades
+num = int(input("Please enter score: "))
 
-# "Please enter a number" is better
-num = input("type down a number:")
-
-# This section doesn't make sense
-'a' >= 75
-'b' >= 60
-'c' >= 35
-'d' <= 35
-# 
-
-# instead of a, b, c, d, do the following
-# if num >= 75:
-#     print("A")
-
-# fix the below
-
-if 'a' == num:
+if num >= 75:
     print("A")
-else:
+elif num >= 60:
     print("B")
-if 'b' == num:
-    print("B")
-else:
-    print("c")
-if 'c' == num:
+elif num >= 35:
     print("C")
-else:
+elif num < 35:
     print("D")
-if 'd' == num:
-    print("D")
-else:
-    print("F")

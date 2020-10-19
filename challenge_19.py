9382047b5a4cdb35f60692afb6560e0b1033a201
Junior <junior@Anakin.local>
2020-10-19 19:51:43 +0700

    I need some help

---
+++ challenge_19.py
@@ -0,0 +1,6 @@
+# ask user input of num
+num = int(input("Please enter a number: "))
+# print times table of number
+for i in range(1, 13):
+    print(num, 'x', i, '=', num*i)
+# start again when done
---
+++ challenge_20.py
@@ -0,0 +1,13 @@
+# ask user for num and repeat until user got 7
+
+numlist = ("7")
+
+num = int(input("Please enter a number: "))
+
+# while num not in numlist:
+#   print("Please try again")
+if num == numlist:
+    print("Well done!!!!!")
+else:
+    print("please retry rhe program")
+# say well done for getting the right number

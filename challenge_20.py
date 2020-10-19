d5fa92ef4bbd44647de880e2dab6c28b9c4c6405
Kimfucious <github@kimford.me>
2020-10-19 20:14:05 +0700

    add help comments

--- challenge_20.py
+++ challenge_20.py
@@ -1,13 +1,16 @@
 # ask user for num and repeat until user got 7
 
-numlist = ("7")
+# lists use [], not ()
+numlist = ("7") # fix me
+
+# you can just set 7 to be a variable, instead of a list.
+# desired_number = 7
 
 num = int(input("Please enter a number: "))
 
-# while num not in numlist:
-#   print("Please try again")
-if num == numlist:
-    print("Well done!!!!!")
-else:
-    print("please retry rhe program")
+# while num is not equal to 7
+# keep asking for user input
+
+# while loop breaks once user input equals 7
+
 # say well done for getting the right number

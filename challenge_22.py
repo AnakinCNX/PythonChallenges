e25c8f0eefdc5f008984e705b5d09e6b2f07429e
Junior <junior@Anakin.local>
2020-10-20 19:51:31 +0700

    complete challenges

---
+++ challenge_22.py
@@ -0,0 +1,6 @@
+# asks the user for the distance (in metres)
+cm = int(input("Please enter a distance in cm: "))
+# asks the user for the time in seconds that a journey was completed in
+time = int(input("Please enter the time will it take to reach there in seconds: "))
+# calculates and outputs the average speed using a function
+print(cm, '/', time, '=', cm/time)

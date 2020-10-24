3a480fa8b42ac32d2bde5b7196e3537cd38ae4ce
Junior <junior@Anakin.local>
2020-10-24 12:59:17 +0700

    complete challenges

---
+++ challenge_24.py
@@ -0,0 +1,22 @@
+def drawline(p, x):
+    for i in range(0, p):
+        print(" ", end=" "),
+    for i in range(0, x):
+        print(".", end=" "),
+    print()
+    return
+
+
+def drawpicture():
+    drawline(1, 3)
+    drawline(1, 5)
+    drawline(1, 6)
+    drawline(1, 3)
+    drawline(1, 8)
+    drawline(1, 2)
+    drawline(1, 9)
+    drawline(1, 7)
+    return
+
+
+drawpicture()

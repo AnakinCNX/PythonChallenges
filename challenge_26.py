4b790f7b124beda7f23cd6a1bb455f1dbe481466
Junior <junior@Anakin.local>
2020-10-25 15:53:23 +0700

    HelpThisProgramIsDoingTheOppisiteThingToDo

---
+++ challenge_26.py
@@ -0,0 +1,26 @@
+name = input("Please enter your name: ")
+# Ask user for name for quiz
+# ask user for the answers for the quiz
+Q1 = input("What is 3+2: ")
+
+
+if Q1 == 5:
+    print("correct")
+else:
+    print("incorrect")
+
+Q2 = input("what is 25*4: ")
+
+
+if Q2 == 100:
+    print("correct")
+else:
+    print("incorrect")
+
+Q3 = input("what is 100/10: ")
+
+
+if Q3 == 10:
+    print("correct")
+else:
+    print("incorrect")

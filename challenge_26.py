326ea0b0700b30427a97679057417fdfdd3e4e60
Kimfucious <github@kimford.me>
2020-11-08 21:40:21 +0700

    add dad comments

--- challenge_26.py
+++ challenge_26.py
@@ -1,8 +1,19 @@
 name = input("Please enter your name: ")
 # Ask user for name for quiz
 # ask user for the answers for the quiz
+
+# Don't use a capital letter when assigning variables.
 Q1 = input("What is 3+2: ")
 
+# You need to write this so that the user is:
+#  1) presented with a question
+#  2) given the chance to respond
+#  3) is asked the next question
+
+#  You can either tell them if each answer is right or wrong, or better
+#  You can keep track of the score, and at the end of the quiz, give them a total score.
+
+# Pay attention to types, because you can't do math with strings.
 
 if Q1 == 5:
     print("correct")

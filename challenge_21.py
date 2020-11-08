a8336f661d07b739044a1cab90896aa235643b02
Kimfucious <github@kimford.me>
2020-11-08 21:21:58 +0700

    add dad comments

--- challenge_21.py
+++ challenge_21.py
@@ -1,8 +1,15 @@
 # Write a program that converts between centimetres, and inches
+# you are mixing things in your assignments here
+# 2.54 [space] cm is incorrect.  You should only assign a string or a number here.
+# you want two vars:  cm_to_inches and inches_to_cm
 inches = 2.54 cm
 cm = 0.393700787 inches
 # asking the user to input a number
 num = int(input("Please enter a number: "))
 # asking the user to choose between converting from centimetres to inches or from inches to centimetres
 
+# you need another input from the user here
+
 # calculating and outputting the result using functions
+
+# what kind of logic would you use to determine what to do with the inputs?

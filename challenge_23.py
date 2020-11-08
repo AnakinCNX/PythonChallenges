ad8685dbc0c1a87cadfe204d3cf1d2ed6b8f4e3a
Kimfucious <github@kimford.me>
2020-11-08 21:32:52 +0700

    add dad comments

--- challenge_23.py
+++ challenge_23.py
@@ -1,3 +1,10 @@
+# fir is a type of tree...
 # ask user for lawn size fir circle in meters
 num = int(input("Please enter the size of the lawn in meters: "))
+# your spelling sucks below
 # used funtion to calculate and print turf needed
+
+# Turf is word for grass (artificial or natural)
+
+# What is the geometric calculation that you would need to describe the amount of turf you will need?
+# As the dimension is a circle you will need to remember how to calculate this for a circle.
